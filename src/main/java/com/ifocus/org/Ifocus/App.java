4c95980c7bded57ba4cc36f6637f28ab86580023
hiremathsachin <sachinhrmth@gmail.com>
2024-12-30 16:17:25 +0530

    adding my changes

---
+++ src/main/java/com/ifocus/org/Ifocus/App.java
@@ -0,0 +1,10 @@
+package com.ifocus.org.Ifocus;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/test/java/com/ifocus/org/Ifocus/AppTest.java
@@ -0,0 +1,19 @@
+package com.ifocus.org.Ifocus;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+       System.out.println("Executing maven framework in jenkins");
+    }
+}
